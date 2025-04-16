9c3650c294acee84633b9a08c09a784e5f68d541
Chrisban media <ihuomachristopher081@gmail.com>
2025-04-16 13:32:22 +0100

    edited update

--- script.js
+++ script.js
@@ -5,34 +5,37 @@ document.addEventListener("DOMContentLoaded", function () {
 
 // })();
 
-document.getElementById("contact-form").addEventListener("submit", function (event) {
+document
+  .getElementById("contact-form")
+  .addEventListener("submit", function (event) {
     event.preventDefault();
 
     // Get form values
-    let  first_name= document.getElementById("name-box").value;
-     let last_name= document.getElementById("last-name").value;
-     let email= document.getElementById("info-box").value;
-     let phone= document.getElementById("info-box").value;
-     let message= document.getElementById("message-box").value;
-    
+    let first_name = document.getElementById("name-box").value;
+    let last_name = document.getElementById("last-name").value;
+    let email = document.getElementById("info-box").value;
+    let phone = document.getElementById("info-box").value;
+    let message = document.getElementById("message-box").value;
+
     let templateParams = {
       from_name: first_name + " " + last_name,
       from_email: email,
       message: message,
       to_name: "Group A",
     };
-    console.log(templateParams)
+    console.log(templateParams);
 
-    emailjs.send("service_ga20tw6", "template_av9o39n", templateParams)
-    .then(function (response) {
-      
-      alert("Message sent successfully!");
-      console.log("SUCCESS!", response.status, response.text);
+    emailjs
+      .send("service_ga20tw6", "template_av9o39n", templateParams)
+      .then(function (response) {
+        console.log("SUCCESS!", response.status, response.text);
+
+        alert("Message sent successfully!");
         document.getElementById("contact-form").reset();
       })
-        .catch (function (error) {
+      .catch(function (error) {
         alert("Failed to send message. Please try again." + error.text);
         console.log("FAILED...", error);
+        alert("Failed to send message. Please try again." + error.text);
       });
-    });
-  
+  });
---
+++ test.js
@@ -0,0 +1,3 @@
+const wlcm = document.getElementById("welcome-text");
+
+wlcm.innerHTML = `<h2 style="color: white">hello world</h2>`;
